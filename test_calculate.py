6e1ce21bc096ec5b25acc7e738c83e6c0692e9f5
Fahri Mert <fmtunckaya@gmail.com>
2025-12-30 12:46:43 +0300

    chore:chore

--- test_calculate.py
+++ test_calculate.py
@@ -1,12 +1,12 @@
 import unittest
-import hesapla
+import calculate
 
 class TestCalculate(unittest.TestCase):
     def test_sum(self):
-        self.assertEqual(hesapla.topla(3, 5), 8)
+        self.assertEqual(calculate.sum(3, 5), 8)
 
     def test_multiple(self):
-        self.assertEqual(hesapla.carp(2, 4), 8)
+        self.assertEqual(calculate.multiple(2, 4), 8)
     
     #code changes for jenkins
 if __name__ == '__main__':
